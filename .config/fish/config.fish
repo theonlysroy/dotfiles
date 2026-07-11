9f4c3d8f0374710756be774a63c2069327c5a469
theonlysroy <tirtha13july2002sonofmp@gmail.com>
2026-07-11 23:22:45 +0530

    fish configs added

---
+++ .config/fish/config.fish
@@ -0,0 +1,41 @@
+set -U __fish_git_prompt_show_informative_status 1
+set -U __fish_git_prompt_showdirtystate 1
+set -U __fish_git_prompt_showuntrackedfiles 1
+set -U __fish_git_prompt_showupstream auto
+
+# aliases
+alias aud="sudo apt update"
+alias aug="sudo apt upgrade"
+alias lab="cd $HOME/Lab"
+alias projects="cd $HOME/Projects"
+alias scripts="cd $HOME/Lab/Scripts"
+alias awslab="cd $HOME/Lab/Aws"
+alias office="cd $HOME/Office"
+alias fe="cd $HOME/Lab/frontend"
+alias be="cd $HOME/Lab/backend"
+alias work="cd $HOME/Backup/Work"
+alias v="vim"
+alias python="python3"
+alias py="python3"
+alias dfn="df -H | grep nvme"
+alias dft="df -H --total | grep total"
+
+# bun
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
+
+# nvm
+set --export NVM_DIR $HOME/.nvm
+
+# opencode
+fish_add_path "$HOME/.opencode/bin"
+
+# java
+set --export JAVA_HOME "/usr/lib/jvm/java-21-openjdk-amd64"
+set --export PATH $JAVA_HOME/bin $PATH
+
+# android sdk
+set --export ANDROID_HOME "$HOME/android_sdk"
+set --export PATH $ANDROID_HOME/cmdline-tools/latest/bin $PATH
+set --export PATH $ANDROID_HOME/platform-tools $PATH
+set --export PATH $ANDROID_HOME/emulator $PATH
